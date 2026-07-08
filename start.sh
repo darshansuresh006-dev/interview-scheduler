de587b9abaaa8948f872eb336ed7d699ff0f6d01
Hema <darshansuresh006@email.com>
2026-07-08 11:52:49 +0530

    Reduce Celery and Gunicorn concurrency to fit free tier memory limits

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-# Start Celery worker in the background
-celery -A interview_scheduler worker --loglevel=info &
+# Start Celery worker in the background (low concurrency to fit free tier memory)
+celery -A interview_scheduler worker --loglevel=info --concurrency=2 --pool=solo &
 
 # Start Celery beat in the background
 celery -A interview_scheduler beat --loglevel=info &
 
 # Start Django with gunicorn in the foreground (keeps the service alive)
-gunicorn interview_scheduler.wsgi --log-file -
+gunicorn interview_scheduler.wsgi --workers=1 --log-file -
